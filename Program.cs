148b625119328b1ddaaa2504a4da9afdc93f547f
Sky1sBloo <ultimate8gaming@gmail.com>
2025-09-18 20:12:48 +0800

    Moved program cs to a class container

--- Program.cs
+++ Program.cs
@@ -1,28 +1,34 @@
-﻿Random random = new Random();
-ProgramSettings? programSettings = null;
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        ProgramSettings? programSettings = null;
 
-try
-{
-    programSettings = ArgumentHandler.HandleArguments(args);
-}
-catch (ArgumentNullException ex)
-{
-    Console.WriteLine(ex.Message);
-    Environment.Exit(1);
-}
+        try
+        {
+            programSettings = ArgumentHandler.HandleArguments(args);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Environment.Exit(1);
+        }
 
-if (programSettings == null)
-{
-    Console.WriteLine("Failed to load program settings");
-    Environment.Exit(1);
-}
+        if (programSettings == null)
+        {
+            Console.WriteLine("Failed to load program settings");
+            Environment.Exit(1);
+        }
 
-try
-{
-    Game game = new Game(programSettings, random);
-    game.StartGame();
-}
-catch (InvalidDataException ex)
-{
-    Console.WriteLine(ex.Message);
+        try
+        {
+            Game game = new Game(programSettings, random);
+            game.StartGame();
+        }
+        catch (InvalidDataException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 }
