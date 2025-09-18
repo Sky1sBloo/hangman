bdd0595da97ab92b632c199ee7983c95a4c099ff
Sky1sBloo <ultimate8gaming@gmail.com>
2025-09-18 20:19:10 +0800

    Modified program cs to use a class rather than top level statement

--- Program.cs
+++ Program.cs
@@ -1,28 +1,37 @@
-﻿Random random = new Random();
-ProgramSettings? programSettings = null;
+﻿public class Program
+{
+    static int Main(string[] args)
+    {
+        Random random = new Random();
+        ProgramSettings? programSettings = null;
 
-try
-{
-    programSettings = ArgumentHandler.HandleArguments(args);
-}
-catch (ArgumentNullException ex)
-{
-    Console.WriteLine(ex.Message);
-    Environment.Exit(1);
+        try
+        {
+            programSettings = ArgumentHandler.HandleArguments(args);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return 1;
+        }
+
+        if (programSettings == null)
+        {
+            Console.WriteLine("Failed to load program settings");
+            return 2;
+        }
+
+        try
+        {
+            Game game = new Game(programSettings, random);
+            game.StartGame();
+        }
+        catch (InvalidDataException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return 3;
+        }
+        return 0;
+    }
 }
 
-if (programSettings == null)
-{
-    Console.WriteLine("Failed to load program settings");
-    Environment.Exit(1);
-}
-
-try
-{
-    Game game = new Game(programSettings, random);
-    game.StartGame();
-}
-catch (InvalidDataException ex)
-{
-    Console.WriteLine(ex.Message);
-}
